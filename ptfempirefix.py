3802f6f4fa3e8494adb4e71a46f36ae817a8c7b9
trenchesofit <42854302+trenchesofit@users.noreply.github.com>
2018-08-30 21:58:56 -0400

    Update ptfempirefix.py

--- ptfempirefix.py
+++ ptfempirefix.py
@@ -1,3 +1,3 @@
 import os
 
-os.system("python -m pip install pydispatch empire pydispatch pydispatcher zlib_wrapper macholib pefile xlutils dropbox pyminifier")
+os.system("python -m pip install iptools pydispatch empire pydispatch pydispatcher zlib_wrapper macholib pefile xlutils dropbox pyminifier")
